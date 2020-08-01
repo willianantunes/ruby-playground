6a41d67a527eb313433ae28edc51558333a91419
Willian Antunes <willian.lima.antunes@gmail.com>
2020-08-01 12:19:09 -0300

    Even a newly created object isn’t a blank slate

--- ticket.rb
+++ ticket.rb
@@ -41,3 +41,13 @@ puts "This ticket is for: #{ticket.event}, at #{ticket.venue}.\n" +
          "The performer is #{ticket.performer}.\n" +
          "The seat is #{ticket.seat}, " +
          "and it costs $#{"%.2f." % ticket.price}"
+
+
+# [:!, :!=, :!~, :<=>, :==, :===, :=~, :__id__, :__send__, :class, :clone, :define_singleton_method,
+# :display, :dup, :enum_for, :eql?, :equal?, :extend, :freeze, :frozen?, :hash, :inspect, :instance_eval,
+# :instance_exec, :instance_of?, :instance_variable_defined?, :instance_variable_get, :instance_variable_set,
+# :instance_variables, :is_a?, :itself, :kind_of?, :method, :methods, :nil?, :object_id, :private_methods,
+# :protected_methods, :public_method, :public_methods, :public_send, :remove_instance_variable, :respond_to?,
+# :send, :singleton_class, :singleton_method, :singleton_methods, :taint, :tainted?, :tap, :then, :to_enum,
+# :to_s, :trust, :untaint, :untrust, :untrusted?, :yield_self]
+p Object.new.methods.sort
