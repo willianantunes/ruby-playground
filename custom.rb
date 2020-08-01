f56cfb92e87f7ee4a58fb649d3a368f80b799afd
Willian Antunes <willian.lima.antunes@gmail.com>
2020-08-01 10:30:46 -0300

    My very first code written in ruby
    Interesting links:
    - https://en.wikibooks.org/wiki/Ruby_Programming/Unit_testing
    - https://www.manning.com/books/the-well-grounded-rubyist-third-edition

---
+++ custom.rb
@@ -0,0 +1,9 @@
+def convert_to_fahrenheit(celsius)
+  return (celsius * 9 / 5) + 32
+end
+
+celsius = 100
+fahrenheit = (celsius * 9 / 5) + 32
+puts "The result is "
+puts fahrenheit
+puts "."
---
+++ test_custom.rb
@@ -0,0 +1,9 @@
+require_relative 'custom'
+require "test/unit"
+
+class MyVeryFirstHonestTest < Test::Unit::TestCase
+  def test_simple
+    self.assert_equal(212, convert_to_fahrenheit(100))
+    assert_equal(212, convert_to_fahrenheit(100))
+  end
+end
