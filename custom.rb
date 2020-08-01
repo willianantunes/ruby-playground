4aa64b8ff3564422782f014f71d83f2a447d7037
Willian Antunes <willian.lima.antunes@gmail.com>
2020-08-01 10:57:10 -0300

    Built-in configs and $: special global variable

--- custom.rb
+++ custom.rb
@@ -7,3 +7,9 @@ fahrenheit = (celsius * 9 / 5) + 32
 puts "The result is "
 puts fahrenheit
 puts "."
+
+p = RbConfig::CONFIG["bindir"]
+puts p
+# The Ruby interpreter’s load path is a list of directories in which it searches for files you ask it to load.
+# You can see the names of these directories by examining the contents of the special global variable $: (dollar-colon)
+puts $:
