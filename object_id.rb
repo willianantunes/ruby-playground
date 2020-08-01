ff4366e54d18e90c9db0562c54c201e09895cb7a
Willian Antunes <willian.lima.antunes@gmail.com>
2020-08-01 12:24:10 -0300

    Every object in Ruby has a unique ID number associated with it

---
+++ object_id.rb
@@ -0,0 +1,5 @@
+obj = Object.new
+puts "The id of obj is #{obj.object_id}."
+str = "Strings are objects too, and this is a string!"
+puts "The id of the string object str is #{str.object_id}."
+puts "And the id of the integer 100 is #{100.object_id}."
