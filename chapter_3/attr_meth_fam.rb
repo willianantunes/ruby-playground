7858e37d21c2f9b4398cd27ae51a51188c4c9a6c
Willian Antunes <willian.lima.antunes@gmail.com>
2020-08-01 17:08:28 -0300

    Using attr_writer

--- chapter_3/attr_meth_fam.rb
+++ chapter_3/attr_meth_fam.rb
@@ -1,15 +1,13 @@
 class RefactoredTicket
   attr_reader :price, :venue, :date
+  attr_writer :price
 
   def initialize(venue, date)
     @venue = venue
     @date = date
   end
-
-  def price=(price)
-    @price = price
-  end
 end
 
 ticket = RefactoredTicket.new "Sample Venue", Time.new
-puts ticket
+ticket.price = 15.6
+puts ticket.price
