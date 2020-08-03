b89e8fa6a6e4bd5655459d4acc2afced549fa2a6
Willian Antunes <willian.lima.antunes@gmail.com>
2020-08-03 19:21:02 -0300

    More samples dealing with self in various contexts

--- chapter_5/honest_self.rb
+++ chapter_5/honest_self.rb
@@ -46,3 +46,81 @@ end
 # C2::M2
 # Back in the outer level of C2:
 # C2
+
+obj = Object.new
+
+def obj.show_me
+  puts "Inside singleton method show_me of #{self}"
+end
+
+obj.show_me
+puts "Back from call to show_me by #{obj}"
+
+# Inside singleton method show_me of #<Object:0x00007f9c798b0278>
+# Back from call to show_me by #<Object:0x00007f9c798b0278>
+
+class C3
+  # class << self instructs the class that the following methods
+  # will be class methods. Notice the end keyword that declares
+  # the end of the class-definition grouping.
+  class << self
+    def x
+      # definition of x
+    end
+
+    def y
+      # definition of y
+    end
+  end
+end
+
+class C4
+  def C4.no_dot
+    puts "As long as self is C, you can call this method with no dot"
+  end
+
+  no_dot
+end
+
+C4.no_dot
+
+# As long as self is C, you can call this method with no dot
+# As long as self is C, you can call this method with no dot
+
+class C5
+  def x
+    puts "This is method 'x'"
+  end
+
+  def y
+    puts "This is method 'y', about to call x without a dot."
+    x
+  end
+end
+
+c = C5.new
+c.y
+
+# This is method 'y', about to call x without a dot.
+# This is method 'x'
+
+class Person
+  attr_accessor :first_name, :middle_name, :last_name
+
+  def whole_name
+    n = first_name + " "
+    n << "#{middle_name} " if middle_name
+    n << last_name
+  end
+end
+
+david = Person.new
+david.first_name = "David"
+david.last_name = "Black"
+puts "David's whole name: #{david.whole_name}"
+david.middle_name = "Alan"
+puts "David's new whole name: #{david.whole_name}"
+
+# David's whole name: David Black
+# David's new whole name: David Alan Black
+
