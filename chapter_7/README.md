78109d6ae65d18baf5b5d740792a5d3c61d4afac
Willian Antunes <willian.lima.antunes@gmail.com>
2020-08-06 13:42:01 -0300

    Popular built-in and standard library classes

---
+++ chapter_7/README.md
@@ -0,0 +1,10 @@
+# Summary
+
+- This chapter covers
+- Literal object constructors
+- Syntactic sugar
+- “Dangerous” and/or destructive methods
+- The `to_*` family of conversion methods
+- Boolean states and objects, and `nil`
+- Object-comparison techniques
+- Runtime inspection of objects’ capabilities
---
+++ chapter_7/popular.rb
@@ -0,0 +1,57 @@
+# Popular built-in classes
+Array
+Binding
+Dir
+Exception
+File
+Float
+Hash
+IO
+Integer
+Kernel
+Math
+Mutex
+Proc
+Process
+Random
+Range
+Rational
+String
+Struct
+Symbol
+Thread
+Time
+
+# Array, Hash, String, and Float are all built-in classes because they’re
+# useful and prevalent go-tos available without so much as a require statement
+
+# By contrast, Date is not a built-in class because one must require 'date' before using it.
+
+# Popular standard library classes
+# Base64
+# Benchmark
+# BigDecimal
+# CSV
+# Date
+# Digest
+# ERB
+# FileUtils
+# JSON
+# Logger
+# Pathname
+# PrettyPrint
+# Prime
+# Psych
+# Rake
+# Rdoc
+# Scanf
+# Set
+# Shell
+# Singleton
+# Socket
+# StringIO
+# Tempfile
+# Time
+# URI
+# WEBrick
+# YAML
