02c162080d38885c6bab07ecd73ad45bddb463cb
Willian Antunes <willian.lima.antunes@gmail.com>
2020-08-06 18:25:20 -0300

    Literal constructors

---
+++ chapter_7/constructors.rb
@@ -0,0 +1,13 @@
+# Some can’t; for example, you can’t create a new instance of the class Integer
+str = String.new
+arr = Array.new
+
+sample_string = "calopsita"
+sample_symbol = :ThisIsASymbol
+sample_symbol_ = :"This is a Symbol with spaces"
+sample_array = [0, 1, 2, 3, 4, 5]
+sample_hash = {"New York": "NY", "Oregon": "OR"}
+sample_range = 1..9
+sample_range_2 = 1...9
+sample_regexp = /([a-z]+)/
+proc_or_lambda = ->(x, y) { x * y }
