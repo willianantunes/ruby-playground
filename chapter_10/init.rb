de179cf0da04a67b19bf31b3f9026abe1750d346
Willian Antunes <willian.lima.antunes@gmail.com>
2020-08-11 19:09:44 -0300

    Enumerability through each and using boolean queries

--- chapter_10/init.rb
+++ chapter_10/init.rb
@@ -46,6 +46,51 @@ end
 # with a whole slew of methods built on top of the each method.
 
 r = Rainbow.new
+# find works by calling each
 y_color = r.find { |color| color.start_with?('y') }
 puts "First color starting with 'y' is #{y_color}." # First color starting with 'y' is yellow.
+
+# Thanks to the false argument, the list includes only the methods defined in the Enumerable module itself.
+# Each of these methods is built on top of each.
+p Enumerable.instance_methods(false).sort # [:all?, :any?, :chain, :chunk, :chunk_while, :collect, :collect_concat, :count, :cycle, :detect, :drop, :drop_while, :each_cons, :each_entry, :each_slice, :each_with_index, :each_with_object, :entries, :filter, :find, :find_all, :find_index, :first, :flat_map, :grep, :grep_v, :group_by, :include?, :inject, :lazy, :map, :max, :max_by, :member?, :min, :min_by, :minmax, :minmax_by, :none?, :one?, :partition, :reduce, :reject, :reverse_each, :select, :slice_after, :slice_before, :slice_when, :sort, :sort_by, :sum, :take, :take_while, :to_a, :to_h, :uniq, :zip]
+
+
+states = ["Louisiana"]
+p states.include?("Louisiana") # true
+p states.all? { |state| state =~ / / } # false
+p states.any? { |state| state =~ / / } # false
+p states.one? { |state| state =~ /West/ } # false
+p states.none? { |state| state =~ /East/ } # trues
+
+states = {"New Jersey" => "NJ",
+          "Connecticut" => "CT",
+          "Delaware" => "DE"}
+
+p states.include?("Louisiana") # false
+p states.all? { |state, abbr| state =~ / / } # false
+p states.one? { |state, abbr| state =~ /West/ } # false
+
+# Generating the entire keys array in advance, rather than walking through
+# the hash that’s already there, is slightly wasteful of memory
+
+p states.keys.all? { |state| state =~ / / } # false
+
+def test_range_thing
+  r = Range.new(1, 10)
+  r.one? { |n| n == 5 }
+  r.none? { |n| n % 2 == 0 }
+  r = Range.new(1.0, 10.0)
+  begin
+    r.one? { |n| n == 5 }
+  rescue => e
+    puts "Following error caught: #{e.message}"
+  end
+  r = Range.new(1, 10.3)
+  1..10.3
+  r.any? { |n| n > 5 }
+  true
+end
+
+test_range_thing
+
 
